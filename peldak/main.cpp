ac6feebedd2d7426df33e142de3bc4799977f0a4
Dékány Róbert Zsolt <teaisawesome@users.noreply.github.com>
2020-01-27 23:26:35 +0100

    Update main.cpp

--- peldak/main.cpp
+++ peldak/main.cpp
@@ -13,3 +13,29 @@ int fun(void* param)
 // ha meg akarjuk tudni a típusát akkor írunk pl. enum típusokat és 2. argumentumként bekérjük a típust, majd a függvénybe az alapján
 // dobjuk vissza az értéket, amelyet képvisel.
 
+/*---pointerek, referenciák, konstruktorok, operatorok---*/
+
+// referenciát tartalmazó konstruktor 
+class A
+{
+   int a;
+   int& b;
+   
+   A(int i = 0) : a(i), b(a) {}
+   
+   // saját copy assignment operator
+   A& operator=(const A& param)
+   {
+      a = param.a;
+      b = param.b;
+      return *this;
+   }
+};
+
+int main()
+{
+   A a;
+   B b;
+   
+   a = b; // error: nem tudja kezelni a default assignment operátor a non-static ref miatt ezért sajátot kell, hogy írjunk!
+}
